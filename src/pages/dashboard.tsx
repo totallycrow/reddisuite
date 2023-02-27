764a457ce2b8cfa5feb7a4a6c45e7474e3bbcc08
Adrian Gajda <adrian.gajda@hotmail.com>
2023-02-27 14:20:49 +0000

    add basic form controls and set simple post submitting data flow

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -10,7 +10,13 @@ export default function Dashboard() {
   const [dataGet, setDataGet] = useState("");
   console.log(dataGet);
 
+  const mutation = api.example.sendPost.useMutation();
+
   const { data: sessionData } = useSession();
+
+  const [title, setTitle] = useState("");
+  const [link, setLink] = useState("");
+  const [sub, setSub] = useState("");
 
   const { data, refetch } = api.example.getSubreddit.useQuery(
     { input: sessionData?.user.token },
@@ -27,13 +33,25 @@ export default function Dashboard() {
     return;
   };
 
+  const sendData = async () => {
+    console.log("ONCLICK");
+    mutation.mutate({
+      token: sessionData?.user.token,
+      title: title,
+      sub: sub,
+      link: link,
+    });
+    console.log("ONCLICK END");
+    return;
+  };
+
   if (session) {
     return (
       <div>
         <h1>Protected Page</h1>
         <p>You can view this page because you are signed in.</p>
 
-        <p>Returned info: {""}</p>
+        {/* <p>Returned info: {""}</p>
         <div>{data ? data[0].text : ""}</div>
         <div>
           <input
@@ -43,6 +61,40 @@ export default function Dashboard() {
           ></input>
           <div></div>
           <button onClick={getData}>Verify</button>
+        </div> */}
+
+        <div>
+          <h2>Submit Your Post</h2>
+          <div>
+            <div>
+              Title:{" "}
+              <input
+                type="text"
+                className="border-2 border-gray-800"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div>
+              Link:{" "}
+              <input
+                type="text"
+                className="border-2 border-gray-800"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+              />
+            </div>
+            <div>
+              Subreddit:{" "}
+              <input
+                type="text"
+                className="border-2 border-gray-800"
+                value={sub}
+                onChange={(e) => setSub(e.target.value)}
+              />
+            </div>
+          </div>
+          <button onClick={() => void sendData()}>Submit</button>
         </div>
       </div>
     );
