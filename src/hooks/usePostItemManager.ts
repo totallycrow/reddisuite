bc9d0583005e5b1b54863c8aa65520a5dbc26d01
Adrian Gajda <adrian.gajda@hotmail.com>
2023-03-22 12:16:40 +0000

    refactor bloadem postItem component logic into hooks

---
+++ src/hooks/usePostItemManager.ts
@@ -0,0 +1,169 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { FormObserver } from "../utils/formObserver";
+import { useFormController } from "./useFormController";
+import { IPostFormValues } from "../components/postSubmission/postItem/PostItem";
+import { useSubredditController } from "./useSubredditController";
+import { useFlairController } from "./useFlairController";
+import { usePostingController } from "./usePostingController";
+import { useFormItemValidation } from "./useFormItemValidation/useFormItemValidation";
+
+export const usePostItemManager = (postConfig: IPostFormValues) => {
+  const [loadingState, setLoadingState] = useState("Idle");
+
+  const formObserver = useMemo(() => FormObserver.getInstance(), []);
+  const isSubmitting = formObserver.isAnyInputSubmitting();
+
+  // Form Controls
+  const { title, setTitle, link, setLink, userInput, setUserInput } =
+    useFormController(postConfig.title, postConfig.link, postConfig.subreddit);
+
+  // *********************** DATA FETCH / POST *************************************
+
+  const {
+    subRedditController,
+    isTitleTagRequired,
+    titleTags,
+    debouncedStatus,
+  } = useSubredditController(userInput, setLoadingState);
+
+  const subData = subRedditController.data ?? {
+    error: "subReddit data not defined",
+  };
+
+  const { selectedFlair, setSelectedFlair, isFlairRequired, flairList } =
+    useFlairController(subData);
+  const { mutationController, sendData, submissionStatus } =
+    usePostingController(
+      title,
+      userInput,
+      link,
+      selectedFlair,
+      setLoadingState
+    );
+
+  const isFormItemValidated = useFormItemValidation(
+    title,
+    isTitleTagRequired,
+    titleTags,
+    link,
+    loadingState,
+    subData,
+    false
+  );
+
+  const isError =
+    formObserver.getFormItemBySubreddit(userInput)?.isError || false;
+
+  console.log(isFormItemValidated);
+
+  // LOADING CHECKS
+  useEffect(() => {
+    if (
+      subRedditController.isLoading ||
+      subRedditController.isFetching ||
+      subRedditController.isRefetching ||
+      subRedditController.isInitialLoading ||
+      submissionStatus === "LOADING"
+    ) {
+      setLoadingState("Loading...");
+      formObserver.updateIdleStatus(userInput, false);
+    } else {
+      setLoadingState("Idle");
+      formObserver.updateIdleStatus(userInput, true);
+    }
+  }, [subRedditController.isLoading]);
+
+  // LIST FOR LOCAL INPUT CHANGES
+  useEffect(() => {
+    postConfig.triggerLocalChange(true);
+
+    if (formObserver.isSubredditInList(userInput)) {
+      console.log("UPDATING");
+
+      const idle =
+        formObserver.getFormItemBySubreddit(userInput)?.isIdle || false;
+
+      const error =
+        formObserver.getFormItemBySubreddit(userInput)?.isError || false;
+
+      formObserver.updateFormItem({
+        title: title,
+        link: link,
+        subreddit: userInput,
+        sendData: sendData,
+        successfullySubmitted: false,
+        isSubmitted: false,
+        isSubmitting: false,
+        validated: isFormItemValidated.isValid,
+        flairID: selectedFlair,
+        isIdle: idle,
+        isError: error,
+      });
+    } else {
+      console.log("CREATE NEW SUBSCRIBER");
+      formObserver.subscribe({
+        sendData: sendData,
+        subreddit: userInput,
+        title: title,
+        link: link,
+        successfullySubmitted: false,
+        isSubmitted: false,
+        isSubmitting: false,
+        validated: isFormItemValidated.isValid,
+        flairID: selectedFlair,
+        isIdle: false,
+        isError: false,
+      });
+    }
+  }, [
+    title,
+    link,
+    userInput,
+    selectedFlair,
+    sendData,
+    subData,
+    isFormItemValidated,
+  ]);
+
+  const formConfig = {
+    title: title,
+    setTitle: setTitle,
+    link: link,
+    setLink: setLink,
+    userInput: userInput,
+    setUserInput: setUserInput,
+    subRedditController: subRedditController,
+    mutationController: mutationController,
+    setSelectedFlair,
+    sendData,
+    submissionStatus,
+    loadingState,
+    isFormItemValidated,
+    isError,
+    isSubmitting,
+    isAnyInputSubmitting: postConfig.isAnyInputSubmitting,
+    setIsAnyInputSubmitting: postConfig.setIsAnyInputSubmitting,
+  };
+
+  const isLoading =
+    submissionStatus === "LOADING" || loadingState === "Loading...";
+  const hasBeenSubmitted =
+    formObserver.getFormItemBySubreddit(userInput)?.isSubmitted ||
+    formObserver.getFormItemBySubreddit(userInput)?.successfullySubmitted;
+
+  const shouldShowSpinner =
+    isLoading ||
+    isSubmitting ||
+    postConfig.isAnyInputSubmitting ||
+    (debouncedStatus === "Loading..." && !hasBeenSubmitted);
+
+  return {
+    formObserver,
+    loadingState,
+    submissionStatus,
+    isFormItemValidated,
+    userInput,
+    shouldShowSpinner,
+    formConfig,
+  };
+};
