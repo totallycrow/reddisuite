6484ce48035bf50e1c77700d0f3ce961e623d153
Adrian Gajda <adrian.gajda@hotmail.com>
2023-03-30 11:51:19 +0100

    disable eslint and ts build errors for deployment testing

--- next.config.mjs
+++ next.config.mjs
@@ -14,5 +14,20 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  // PROD TEST // TO BE REMOVED
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 export default config;
