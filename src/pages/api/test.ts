cd4418a4d6f8c2f7ab50ea2eed5f27d2d15ee3e9
Adrian Gajda <adrian.gajda@hotmail.com>
2023-04-11 10:50:45 +0100

    modify test api

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,20 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export default async function handler(req: NextRequest, res: NextResponse) {
   console.log("test");
   console.log(req.body);
+  console.log(req.body.test);
 
   // await fetch("https://dummyjson.com/products/1")
   //   .then((res) => res.json())
   //   .then((json) => console.log(json));
 
-  // res.status(200).json({ res: req.body.test });
-
-  try {
-    const json = await req.json();
-    console.log({ json });
-
-    return new Response(JSON.stringify(json), { status: 200 });
-  } catch (e) {
-    console.log(e);
-    return new Response(null, { status: 400, statusText: "Bad Request" });
-  }
+  res.status(200).json({ res: req.body.test });
 }
